5ee505e455d9c7e8ec0ed30152286de67b0fcb66
divyaroja2008-maker <divyaroja2008@gmail.com>
2026-07-03 21:04:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,102 @@
-function downloadCSV() {
+if (
+    localStorage.getItem("login")
+    !== "true"
+) {
 
-    let csv =
-`ID,Name,Department,Activity,Attendance\n`;
+    window.location.href =
+        "login.html";
+}
 
-    students.forEach((s) => {
+let students = [];
 
-        csv +=
-`${s.id},
-${s.name},
-${s.department},
-${s.activity},
-${s.attendance}\n`;
+function addStudent() {
+
+    let name =
+        document.getElementById(
+            "studentName"
+        ).value;
+
+    let department =
+        document.getElementById(
+            "department"
+        ).value;
+
+    students.push({
+
+        name: name,
+
+        department: department,
+
+        attendance: "Not Marked"
     });
 
-    let blob =
-        new Blob([csv],
-        { type: 'text/csv' });
+    displayStudents();
+}
 
-    let url =
-        window.URL.createObjectURL(blob);
+function displayStudents() {
 
-    let a =
-        document.createElement("a");
+    let table =
+        document.getElementById(
+            "studentTable"
+        );
 
-    a.href = url;
+    table.innerHTML = "";
 
-    a.download =
-        "attendance_report.csv";
+    students.forEach((s, index) => {
 
-    a.click();
+        table.innerHTML += `
+
+        <tr>
+
+            <td>${s.name}</td>
+
+            <td>${s.department}</td>
+
+            <td>${s.attendance}</td>
+
+            <td>
+
+                <button onclick="markPresent(${index})">
+
+                    Present
+
+                </button>
+
+                <button onclick="markAbsent(${index})">
+
+                    Absent
+
+                </button>
+
+            </td>
+
+        </tr>
+        `;
+    });
+}
+
+function markPresent(index) {
+
+    students[index].attendance =
+        "Present";
+
+    displayStudents();
+}
+
+function markAbsent(index) {
+
+    students[index].attendance =
+        "Absent";
+
+    displayStudents();
+}
+
+function logout() {
+
+    localStorage.removeItem(
+        "login"
+    );
+
+    window.location.href =
+        "login.html";
 }
